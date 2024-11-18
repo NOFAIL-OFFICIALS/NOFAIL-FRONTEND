669c642c512ac832e10b8e89319b1e8552bd5104
Joseph Adedeji Adewunmi <adedejiosvaldo@gmail.com>
2024-11-19 00:02:14 +0100

    Feature (Auth) -> Connected to the auth backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,57 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import UserHome from "./pages/UserHome"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import ProtectedRoute from "./components/ProtectedRoute"
-import Layout from './components/dashboard/Layout'
-import Inventory from './pages/Inventory'
-
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserHome from "./pages/UserHome";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/dashboard/Layout";
+import Inventory from "./pages/Inventory";
+import toast, { Toaster } from "react-hot-toast";
 function Logout() {
-  localStorage.clear()
-  return <Navigate to="/login" />
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+  localStorage.clear();
+  return <Register />;
 }
-
 
 function App() {
   return (
-    <BrowserRouter   future={{
-      v7_relativeSplatPath: true,
-    }}>
-    
-      <Routes>
-        
-        <Route
-          path="/"
-          element={
-              <Home />
-          }
-        />
-        <Route path="/home" element={<Home /> }/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="/notfound">
-          <Route path="*" element={<NotFound />} />
-        </Route>
-
-        <Route path="/dashboard" element={
-          // <ProtectedRoute>
-          <Layout />} >
-        <Route index element={<UserHome />} />
-        <Route path="inventory" element={<Inventory />} />
-
-
-         {/* </ProtectedRoute> */}
+    <>
+      <Toaster position="top-right" />
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="/notfound">
+            <Route path="*" element={<NotFound />} />
           </Route>
 
-
-         
-      </Routes>
-
-    </BrowserRouter>
-  )
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<UserHome />} />
+            <Route path="inventory" element={<Inventory />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
